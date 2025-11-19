0bcd10383c3741b8b5b15699e8176fbd23102a5a
ANNA ALYCIA RIBEIRO DE FREITAS ROCHA <54420708824@imaccommand014.local>
2025-11-19 11:58:29 -0300

    exercicio02

---
+++ exercicio02.js
@@ -0,0 +1,36 @@
+let Semanas = prompt("Digite um dia da semana ")
+switch (Semanas) {
+    case "1":
+        console.log("Domingo")
+        break;
+
+    case "2":
+        console.log("Segunda-Feira")
+        break;
+
+    case "3":
+        console.log("Terça-Feira")
+        break;
+
+
+    case "4":
+        console.log("Quarta-Feira")
+        break;
+
+    case "5":
+        console.log("Quinta-Feira")
+        break;
+
+    case "6":
+        console.log("Sexta-Feira")
+        break;
+
+    case "7":
+        console.log("Sabádo")
+        break;
+
+
+    default:
+        console.log("Não há esse dia na semana!!!!")
+        break;
+}
