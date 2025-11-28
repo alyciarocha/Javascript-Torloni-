fe8bbfdba26087da909273cb2f819611764b72b1
ANNA ALYCIA RIBEIRO DE FREITAS ROCHA <54420708824@imaccommand014.local>
2025-11-28 11:23:27 -0300

    exercicio concluidos

---
+++ exercicio07.js
@@ -0,0 +1,8 @@
+// 7. Faça um programa que receba um número do usuário e mostre
+// no console a contagem regressiva até 0.
+
+let numeroEscolhido = parseInt(prompt( "Digite um número possitivo para contagem"))
+   while( numeroEscolhido > -1 ){
+    console.log (numeroEscolhido);
+    numeroEscolhido--
+   }
---
+++ exercicio08.js
@@ -0,0 +1,12 @@
+// Faça um programa que receba a idade do usuário, enquanto a
+// resposta do usuário for menor que 18 anos continue a solicitar a
+// idade e mostre uma mensagem quando a idade do usuário for
+// maior que 18.
+
+let idadeUsuario = parseInt( prompt(" Digite sua idade aqui"));
+while( idadeUsuario >18)
+    idadeUsuario = parseInt ( prompt("Você precisa ter mais de 18 anos para acessar"))
+
+if (informesuaidade >= 18) {
+alert("Você é maior de iadade, pode acessar")
+}
